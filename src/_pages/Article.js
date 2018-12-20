86d2b52dcdbad365efc6a68fa0d9352b9ef6aae3
354224080@qq.com <huyongdi525>
2018-12-20 18:11:37 +0800

    实现读取md文件

--- src/_pages/Article.js
+++ src/_pages/Article.js
@@ -1,50 +1,64 @@
 import React, {Component} from 'react';
 import View from './view'
 import axios from 'axios'
-import { Pagination } from 'antd';
+import {Pagination} from 'antd';
 
 export default class Article extends Component {
   state = {
+    mdArr: [],//文件列表
+    currentPage: 1,//当前页currentPage
     currentArticle: {
-      title: '',
-      content: '',
+      title: '', //要展示文件的标题
+      content: '', //要展示文件的内容
     }
   }
 
   //获取 public/markdown下的md文件
-  getMdFile = ()=>{
-    let articleArr = []
+  getMdFile = () => {
+    let mdArr = []
     axios.get('markdown').then((resp) => {
       resp.data.forEach(async (item) => {
+        console.log(1111);
         let respMd = (await axios.get(`markdown/${item}`)).data
-//        console.log(respMd);
-//        respMd = respMd.substring(respMd.indexOf('--- hyd'),respMd.length)
-        Promise.resolve({
+        console.log(2222);
+        mdArr.push({
           title: item.substring(0, item.indexOf('.md')),
           content: respMd
-        }).then((a) => {
-          articleArr.push(a)
         })
       })
-      setTimeout(() => {
-        console.log(articleArr);
-        this.setState({
-          currentArticle: articleArr[0]
-        })
+      console.log(mdArr);
+      this.setState({mdArr}, () => {
+        console.log(this.state.mdArr)
+        this.showArticle()
       })
     })
   }
 
-  //页码改变
-  onPageChange = (page)=>{
-    console.log(page);
+  //通过页码对应文章填充到页面
+  showArticle = () => {
+    Promise.resolve(this.state.mdArr[this.state.currentPage - 1]).then((currentArticle) => {
+      this.setState({currentArticle})
+    })
   }
+
+  //页码改变
+  onPageChange = (currentPage) => {
+    this.setState({currentPage}, () => {
+      this.showArticle()
+    })
+  }
+
   componentDidMount() {
-   this.getMdFile()
+    this.getMdFile()
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (JSON.stringify(nextState.currentArticle) !== JSON.stringify(this.state.currentArticle)) return true
+    return false
   }
 
   render() {
-    const {currentArticle} = this.state
+    const {currentArticle, mdArr, currentPage} = this.state
     return (
         <div className='articleContent'>
           <div className='articleLeft'>
@@ -54,7 +68,7 @@ export default class Article extends Component {
               <div>随笔(1)</div>
             </div>
             <div className='pageContent'>
-              <Pagination simple defaultCurrent={1} total={50} onChange={this.onPageChange}/>,
+              <Pagination simple current={currentPage} total={mdArr.length} pageSize={1} onChange={this.onPageChange}/>,
             </div>
           </div>
           <div className='articleRight'>
